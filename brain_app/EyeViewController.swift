846cc9a75609ba11525763fa2cd934cbd16742c8
Cristina <cristina.bianca1609@gmail.com>
2018-06-09 18:21:04 +0100

    Initial set-up
    Initial app

---
+++ brain_app/EyeViewController.swift
@@ -0,0 +1,23 @@
+//
+//  EyeViewController.swift
+//  brain_app
+//
+//  Created by Cristina Bianca Denk Florea on 09/06/2018.
+//  Copyright © 2018 Cristina Bianca Denk Florea. All rights reserved.
+//
+
+import UIKit
+
+class EyeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    @IBAction func goBack(_ sender: Any) {
+        dismiss(animated: true, completion: ({}))
+    }
+    
+}
