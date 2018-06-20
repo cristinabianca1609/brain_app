1af29e9270e7438edf74e6fd5b686af690acaf69
Cristina <cristina.bianca1609@gmail.com>
2018-06-20 23:41:15 +0300

    Front page, 2 more views
    Added a list on the first page with all the buttons to be used.
    Added a view controller for the 2nd and 3rd button.
    Changed the position of the Back button - not animated yet.

--- brain_app/EyeViewController.swift
+++ brain_app/EyeViewController.swift
@@ -10,14 +10,7 @@ import UIKit
 
 class EyeViewController: UIViewController {
 
-    override func viewDidLoad() {
-        super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
-    }
-
-    @IBAction func goBack(_ sender: Any) {
-        dismiss(animated: true, completion: ({}))
-    }
+   
+    
     
 }
